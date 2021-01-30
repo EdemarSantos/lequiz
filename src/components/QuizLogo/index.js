c3e4de96364eb44c0568f3c8b0b3d84648eda2e9
EdemarSantos <edemarsantos@gmail.com>
2021-01-29 23:39:20 -0300

    typo

--- src/components/QuizLogo/index.js
+++ src/components/QuizLogo/index.js
@@ -7,7 +7,7 @@ const textoDaLogo='Le Quiz';
 function Logo({ className }) {
   return (
     <svg height="100" width="100">
-      <circle cx="50" cy="50" r="40" stroke="#9c27b0" stroke-width="4" fill="#ba68c8" />
+      <circle cx="50" cy="50" r="40" stroke="#9c27b0" strokeWidth="4" fill="#ba68c8" />
       <ellipse cx="50" cy="50" rx="50" ry="25" fill="#ba68c8" />
       <text x="25" y="55" fill="#fff">
         {textoDaLogo}
